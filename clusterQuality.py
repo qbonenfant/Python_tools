c0da533b5b1f4a2b4b6d54b994e239e5f9c1c02f
TheGTeck <quentin.bonenfant@gmail.com>
2020-03-09 06:40:40 +0100

    Adjusted cluster quality script to use read list as 2nd argument.

--- clusterQuality.py
+++ clusterQuality.py
@@ -11,6 +11,17 @@ import warnings
 warnings.filterwarnings("ignore")
 
 clusterFile = sys.argv[1]  # tabbed cluster file
+read_name_file = ""
+if(len(sys.argv) > 2):
+    read_name_file = sys.argv[2]
+
+read_name_assoc = []
+if(read_name_file != ""):
+    with open(read_name_file) as f:
+        for line in f:
+            line = line.rstrip("\n")
+            read_name_assoc.append(line)
+
 
 nbCluster = 0
 nbElem = 0
@@ -25,11 +36,17 @@ with open(clusterFile) as f:
         elements = cluster.rstrip("\n").split("\t")
         cl_size = 0
         for el in elements:
-            # "truth"
-            tag = el.split("_")[-1]
-            nodes.add("_".join(el.split("_")[:-1]))
 
-            if(not tag in genes):
+            # Should check for name type...
+            if(read_name_assoc):
+                read_name = read_name_assoc[int(el)]
+            else:
+                read_name = el
+
+            # "truth"
+            tag = read_name.split("_")[-1]
+            nodes.add("_".join(read_name.split("_")[:-1]))
+            if(tag not in genes):
                 genes.append(tag)
 
             # adding "thruth" label
@@ -44,6 +61,7 @@ with open(clusterFile) as f:
         nbElem += len(elements)
 
 
+
 print("Number of clusters:", nbCluster, sep="\t")
 print("Singletons: ", single, sep="\t")
 print("Total number of elements", nbElem, sep="\t")
@@ -52,24 +70,22 @@ print("Total number of elements", nbElem, sep="\t")
 ##############################################################################
 # Metrics
 #
-print(genes)
+#  print(genes)
 print(metrics.classification_report(labels_true, labels_pred))#, labels=genes))
 
 # # Adjusted Rand Index
-# ari = metrics.adjusted_rand_score(labels_true, labels_pred)
-# print("Adjusted Rand Index :", ari, sep="\t")
+ari = metrics.adjusted_rand_score(labels_true, labels_pred)
+print("Adjusted Rand Index :", ari, sep="\t")
 
 
 # # Homogeneity, completeness and V-measure
-# h, c, vm = metrics.homogeneity_completeness_v_measure(labels_true, labels_pred)
-# print("Homogeneity :", h, sep="\t")
-# print("Completeness :", c, sep="\t")
-# print("V-measure :", vm, sep="\t")
+h, c, vm = metrics.homogeneity_completeness_v_measure(labels_true, labels_pred)
+print("Homogeneity :", h, sep="\t")
+print("Completeness :", c, sep="\t")
+print("V-measure :", vm, sep="\t")
 
 # acc = metrics.accuracy_score(labels_true, labels_pred)
 # print("Accuracy :", acc, sep="\t")
-
-
 
 
 # Precision, recall, fscore
